3f3612482c408661d339ce9e9791d5c1994903cc
Manal <manal.ahmadi@uir.ac.ma>
2023-03-21 13:39:46 -0600

    resolve conflicts

--- src/test/java/BMICalculatorTest.java
+++ src/test/java/BMICalculatorTest.java
@@ -41,29 +41,4 @@ public class BMICalculatorTest {
         String actual4 = BMICalculator.getCategory(35.5);
         assertEquals(expected4, actual4);
     }
-
-
-    @Test
-    public void testPPPetCategory() {
-        // Test case 1: BMI = 17.5 -> Underweight
-        String expected1 = "Underweight";
-        String actual1 = BMICalculator.getCategory(17.5);
-        assertEquals(expected1, actual1);
-
-        // Test case 2: BMI = 23.5 -> Normal weight
-        String expected2 = "Normal weight";
-        String actual2 = BMICalculator.getCategory(23.5);
-        assertEquals(expected2, actual2);
-
-        // Test case 3: BMI = 27.5 -> Overweight
-        String expected3 = "Overweight";
-        String actual3 = BMICalculator.getCategory(27.5);
-        assertEquals(expected3, actual3);
-
-        // Test case 4: BMI = 35.5 -> Obese
-        String expected4 = "Obese";
-        String actual4 = BMICalculator.getCategory(35.5);
-        assertEquals(expected4, actual4);
-    }
-
 }
